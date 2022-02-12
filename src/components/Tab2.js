efbff63fd221447e3c75ccaa8720bbc3567840f1
amodh.pandey64 <amodh.pandey64@gmail.com>
2022-02-12 12:30:32 +0530

    Comment Resuable

--- src/components/Tab2.js
+++ src/components/Tab2.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Component} from 'react';
 import {Text, View} from 'react-native';
+import {Comment} from './common'
 
 const Tab1 = () => {
   return (
     <View>
-      <Text style={{fontSize: 30, fontWeight: 'bold'}}>Chat</Text>
-      <Text>Tested</Text>
+     <Comment />
     </View>
   );
 };
---
+++ src/components/common/Comment.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Comment = ({
+  user = '847288AGV',
+  comment = 'This is me Hello This is me Hello This is me Hello This is me Hello This is me Hello ',
+  createdAt = '4-7-21',
+}) => {
+  const {container, username, text, time} = styles;
+  return (
+    <View style={container}>
+      <Text style={username}>{user}</Text>
+      <Text style={text}>{comment}</Text>
+      <View style={{flexDirection: 'row', justifyContent: 'flex-end'}}>
+        <Text style={time}>{createdAt}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '90%',
+    alignSelf: 'center',
+    backgroundColor: '#d9d1d0',
+    elevation: 5,
+    borderRadius: 10,
+    margin: 10,
+    padding: 5,
+  },
+  username: {
+    fontWeight: 'bold',
+    marginLeft: 3,
+  },
+  text: {
+    fontSize: 20,
+    color: 'black',
+    opacity: 0.8,
+    margin: 5,
+  },
+  time: {
+    fontSize: 15,
+    marginRight: 10,
+  },
+});
+
+export {Comment};
