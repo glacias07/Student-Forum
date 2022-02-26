4c6423ef0d87f7fd21a2d4e46b82f39e13eccf4c
amodh.pandey64 <amodh.pandey64@gmail.com>
2022-02-26 12:46:02 +0530

    Save

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -1,14 +1,67 @@
 import React from 'react';
 import {Component} from 'react';
-import {Text, View, TouchableOpacity} from 'react-native';
+import {Text, View, TouchableOpacity, FlatList} from 'react-native';
+import ChatItem from './common/ChatItem';
+
+const data = [
+  {
+    id: '1',
+    userName: 'Jenny Doe',
+    userImg: 'https://picsum.photos/id/1/200/300',
+    messageTime: '4 mins ago',
+    messageText:
+      'Hey there, this is my test for a post of my social app in React Native.',
+  },
+  {
+    id: '2',
+    userName: 'John Doe',
+    userImg: 'https://picsum.photos/id/2/200/300',
+    messageTime: '2 hours ago',
+    messageText:
+      'Hey there, this is my test for a post of my social app in React Native.',
+  },
+  {
+    id: '3',
+    userName: 'Ken William',
+    userImg: 'https://picsum.photos/id/6/200/300',
+    messageTime: '1 hours ago',
+    messageText:
+      'Hey there, this is my test for a post of my social app in React Native.',
+  },
+  {
+    id: '4',
+    userName: 'Selina Paul',
+    userImg: 'https://picsum.photos/id/9/200/300',
+    messageTime: '1 day ago',
+    messageText:
+      'Hey there, this is my test for a post of my social app in React Native.',
+  },
+  {
+    id: '5',
+    userName: 'Christy Alex',
+    userImg: 'https://picsum.photos/id/11/200/300',
+    messageTime: '2 days ago',
+    messageText:
+      'Hey there, this is my test for a post of my social app in React Native.',
+  },
+];
 
 const ChatScreen = ({navigation}) => {
   return (
-    <View>
-      <TouchableOpacity onPress={()=>navigation.navigate('Personal Message')} >
-      <Text>Hello</Text>
-     </TouchableOpacity>
-    </View>
+    <FlatList
+      DATA={data}
+      renderItem={({item}) => (
+        <>
+        <Text>Hello</Text>
+        <ChatItem
+          userImg={item.userImg}
+          userName={item.userName}
+          messageText={item.messageText}
+          messageTime={item.messageTime}
+        />
+        </>
+      )}
+    />
   );
 };
 
---
+++ src/components/common/ChatItem.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {StyleSheet, View, Text, Image,Dimensions,TouchableOpacity} from 'react-native';
+import {CustomText} from './';
+
+const ChatItem = props => {
+  const {id, userName, userImg, messageTime, messageText} = props;
+  return (
+    <>
+      <TouchableOpacity onPress={()=>console.log("Pressed")} style={styles.container}>
+        <View style={styles.leftContainer}>
+          <Image source={{uri: userImg}} style={styles.avatar} />
+          <View style={styles.midContainer}>
+            <Text style={styles.username}>{userName}</Text>
+            <Text style={styles.lastMessage} numberOfLines={1}>
+              {messageText}
+            </Text>
+          </View>
+        </View>
+
+        <View>
+          <Text style={styles.time}>
+            {messageTime}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        width: Dimensions.get('window').width,
+        justifyContent: "space-between",
+        padding: 10,
+    
+      },
+      leftContainer: { flexDirection: "row" },
+      midContainer: { justifyContent: "space-around"},
+      avatar: {
+        height: 60,
+        width: 60,
+        marginRight: 15,
+        borderRadius: 50,
+      },
+      username: {
+        fontWeight: "bold",
+        fontSize: 16,
+      },
+      lastMessage: {
+        fontSize: 16,
+        color: "grey",
+    
+      },
+      time: {
+        fontSize: 14,
+        color: "grey",
+        // marginRight:100
+      
+      },
+});
+
+export default ChatItem;
