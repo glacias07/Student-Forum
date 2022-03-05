861793c686fc3cc8136a99ab4a523f99657285cf
shriyans <shriyans.n@somaiya.edu>
2022-03-05 21:32:06 +0530

    one more

--- src/components/PostComment.js
+++ src/components/PostComment.js
@@ -65,7 +65,7 @@ const PostComment = props => {
     console.log('Our method of async await',comment[0]);
     comment_object = comment[0]
     comment_object.replies = new_replylist.map(reply => reply);
-    console.log("Isko kehte hai jugad",comment_object)
+     console.log("Isko kehte hai jugad",comment_object)
     updatePostComments([...commentList, comment_object], route.params.post_id);
     navigation.goBack()
   };
@@ -77,7 +77,7 @@ const PostComment = props => {
         onChangeText={reply => {
           setReply(reply.replace(/^\s+|\s+$/g, ''));
         }}
-        placeholder="Your reply"
+        placeholder="Add your Reply ..."
         style={{fontSize: 17, margin: 10}}
       />
 
@@ -95,3 +95,103 @@ const mapStateToProps = state => {
 };
 
 export default connect(mapStateToProps, {})(PostComment);
+
+// import {replyListSet} from '../actions/PostScreenActions';
+// const PostComment = props => {
+//   const {route, username, replyListSet, replyList} = props;
+//   const [reply, setReply] = useState('');
+//   const {user, updatePostCommentReplies} = useContext(AuthContext);
+//   //   const [replyList, setReplyList] = useState([]);
+
+//   // const submitReply = Comment => {
+//   //   updatePostComments(
+//   //     [
+//   //       ...postCommentList,
+//   //       {
+//   //         reply_user_id: user.uid,
+//   //         reply_id: user.uid + moment().format(),
+//   //         reply: reply,
+//   //         username: username,
+//   //         reply_time: moment().format(),
+//   //       },
+//   //     ],
+//   //     route.params.post_id,
+//   //   );
+
+//   //  setReply('')
+//   // };
+//   // useEffect(() => {
+//   //   const k = route.params.comment_replies;
+//   //   replyListSet(k);
+//   // }, []);
+
+//   // const addReplies = async reply => {
+//   //   console.log('Checking ', reply, 'Old replies', replyList);
+//   //   const new_replies = [
+//   //     ...replyList,
+//   //     {
+//   //       reply_user_id: user.uid,
+//   //       reply_id: user.uid + moment().format(),
+//   //       reply: reply,
+//   //       username: username,
+//   //       reply_time: moment().format(),
+//   //     },
+//   //   ];
+
+//   //   await justForTesting(new_replies);
+
+//     // updatePostCommentReplies(
+//     //   route.params.allCommentList.filter(obj => obj.comment_id != route.params.comment_id),
+//     //   route.params.post_id,
+//     //   route.params.comment,
+//     //   route.params.comment_id,
+//     //   route.params.comment_time,
+//     //   route.params.comment_user_id,
+//     //   ['replyList', 'this is it', 'nice welldone '],
+//     //   // replyList,
+//     //   route.params.nameOfUser,
+//     // );
+//     console.log('Final', replyList);
+//   };
+
+//   const justForTesting = async new_replies => {
+//     replyListSet(new_replies);
+//   };
+
+//   const findAndUpdate = Comments => {
+//     const extracted_comment = Comments.filter(
+//       comment => comment.id === route.params.comment_id,
+//     );
+//     extracted_comment.replies = ['New Reply'];
+//     updatePostCommentReplies(
+//       route.params.allCommentList,
+//       route.params.post_id,
+//       extracted_comment,
+//     );
+//   };
+
+//   return (
+//     <View>
+//       <TextInput
+//         multiline={true}
+//         onChangeText={reply => {
+//           setReply(reply.replace(/^\s+|\s+$/g, ''));
+//         }}
+//         placeholder="Add your Reply ..."
+//         style={{fontSize: 17, margin: 10}}
+//       />
+//       <Button title="Submit" onPress={() => addReplies(reply)} />
+//       {/* <Button title="Submit" onPress={() => replyListSet(new_replies)} /> */}
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({});
+
+// const mapStateToProps = state => {
+//   return {
+//     username: state.postListing.username,
+//     replyList: state.postListing.reply_list,
+//   };
+// };
+// export default connect(mapStateToProps, {replyListSet})(PostComment);
