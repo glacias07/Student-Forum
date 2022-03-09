86cc4b8d2219bdcabc19ff90efe1d03a924a376d
amodh.pandey64 <amodh.pandey64@gmail.com>
2022-03-09 18:27:30 +0530

    Save

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -1,8 +1,9 @@
 import React, {useContext, useEffect, useState} from 'react';
-import {Text, View, TouchableOpacity, FlatList} from 'react-native';
+import {Text, View, TouchableOpacity, FlatList,Image} from 'react-native';
 import ChatItem from './common/ChatItem';
 import {AuthContext} from '../routes/AuthProvider';
 import {connect} from 'react-redux';
+import { CustomText } from './common';
 
 const data = [
   {
@@ -50,38 +51,66 @@ const data = [
 const ChatScreen = ({navigation, route, username}) => {
   const {findUser} = useContext(AuthContext);
   const [friendsList, setFriendsList] = useState();
-  const [myData, setMyData] = useState()
+  const [myData, setMyData] = useState();
   const fetchData = async () => {
     const user = await findUser(username);
     setFriendsList(user.friends);
-    setMyData(user)
+    setMyData(user);
     // console.log(user);
   };
   useEffect(() => {
     fetchData();
   }, [myData]);
   return (
-    <FlatList
-      data={friendsList}
-      contentContainerStyle={{marginTop:10,}}
-      renderItem={({item}) => (
-        <ChatItem
-        style={{marginBottom: 10}}
-          chatOnPress={() => {
-            navigation.navigate('Personal Message', {
-              friendData: item,
-              myData: myData,
-              username:item.username
-            });
-          }}
-          navigation={navigation}
-          userImg={item.avatar}
-          userName={item.username}
-          messageText={item.messageText}
-          messageTime={item.messageTime}
+    <>
+      {friendsList!=undefined ? (
+        <FlatList
+          data={friendsList}
+          contentContainerStyle={{marginTop: 10}}
+          renderItem={({item}) => (
+            <ChatItem
+              style={{marginBottom: 10}}
+              chatOnPress={() => {
+                navigation.navigate('Personal Message', {
+                  friendData: item,
+                  myData: myData,
+                  username: item.username,
+                });
+              }}
+              navigation={navigation}
+              userImg={item.avatar}
+              userName={item.username}
+              messageText={item.messageText}
+              messageTime={item.messageTime}
+            />
+          )}
         />
+      ) : (
+        <View style={{backgroundColor: 'white', height: 200,flex:1}}>
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 120,
+            }}>
+            <Image
+              style={{height: 80, width: 80, marginBottom: 10}}
+              source={require('../assets/images/confused.png')}
+            />
+            <CustomText
+              text="No Chats Yet !"
+              textSize={20}
+              textWeight={200}
+            />
+            <CustomText
+              text="Why don't you start one?"
+              textSize={15}
+              textWeight={600}
+            />
+          </View>
+        </View>
       )}
-    />
+    </>
   );
 };
 
